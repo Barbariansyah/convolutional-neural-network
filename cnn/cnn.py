686485aaee717c5afaa801de77a28db4890b667b
Barbariansyah <bariansyahi@gmail.com>
2020-09-15 23:03:17 +0700

    div by stride

--- cnn/cnn.py
+++ cnn/cnn.py
@@ -1,5 +1,5 @@
 import numpy as np
-from .layers import Layer
+from layers import *
 
 class MyCnn(object):
     def __init__(self):
@@ -10,5 +10,20 @@ class MyCnn(object):
         self.layers.append(layer)
         return None
 
-    def feed_forward(self, input: np.array):
-        return None
+    def feed_forward(self, inp: list):
+        temp = inp
+        for layer in self.layers:
+            temp = layer.call(temp)
+        return temp
+
+if __name__ == "__main__":
+    cnn = MyCnn()
+    inp = []
+    for i in range(6):
+        inp.append(np.random.randint(0, 5, size=(6,6)))
+    print(inp)
+
+    cnn.add(Pooling([2,2], 2, 'avg'))
+    res = cnn.feed_forward(inp)
+
+    print(res)
