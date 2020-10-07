75b6e473a2eef0b991a293fe6b75eb21731d5236
Barbariansyah <bariansyahi@gmail.com>
2020-10-07 23:53:18 +0700

    softmax

--- cnn/common.py
+++ cnn/common.py
@@ -5,11 +5,18 @@ def relu(x: float) -> float:
     return x if x > 0 else 0
 
 def softmax(inp: List[np.array]) -> list:
+    x = np.concatenate(inp, axis=0)
     e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum()
+    res = e_x / e_x.sum()
+    res = [np.array([n]) for n in res]
+    return res
 
 def calculate_de_dnet_last_layer(out: List[np.array]) -> List[np.array]:
     pass
 
 def calculate_average_partial_error(batch_partial_error: List[List[List[np.array]]]) -> List[List[np.array]]:
+    #List Data
+    #List layers
+    #List FMs
+    #np.array FM
     pass
