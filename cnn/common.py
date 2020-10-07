cb14aa83afdc5732f8d6f460ccd9ea1fa712681e
Barbariansyah <bariansyahi@gmail.com>
2020-10-08 00:47:25 +0700

    calculate de dnet

--- cnn/common.py
+++ cnn/common.py
@@ -11,8 +11,14 @@ def softmax(inp: List[np.array]) -> list:
     res = [np.array([n]) for n in res]
     return res
 
-def calculate_de_dnet_last_layer(out: List[np.array]) -> List[np.array]:
-    pass
+def calculate_de_dnet_last_layer(out: List[np.array], target_class: int) -> List[np.array]:
+    res = []
+    for j, p in enumerate(out):
+        if j == target_class:
+            res.append(np.array([-(1-p[0])]))
+        else:
+            res.append(np.array([p[0]]))
+    return res
 
 def calculate_average_partial_error(batch_partial_error: List[List[List[np.array]]]) -> List[List[np.array]]:
     #List Data
